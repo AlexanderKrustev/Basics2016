106e5e826a00014b385c3caeabd7a460eb838b54
AlexanderKrustev <a.krustev@steni.bg>
2016-02-29 10:18:47 +0200

    Exam Preparation

---
+++ Exams/26 April 2015/BookProblem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalBookPages = int.Parse(Console.ReadLine());
+            int campingDaysPerMonth = int.Parse(Console.ReadLine());
+            int pagesPerDay = int.Parse(Console.ReadLine());
+
+            int normalDaysPerMonth = 30 - campingDaysPerMonth;
+            double pagesPerMonth = normalDaysPerMonth*pagesPerDay;
+
+            if (pagesPerMonth <= 1)
+            {
+                Console.WriteLine("never");
+            }
+            else
+            {
+                double totalMonths = totalBookPages/pagesPerMonth; 
+                double years = totalMonths/12.0;
+                double months = totalMonths%12.0;
+
+                Console.WriteLine("{0} years {1} months", Math.Floor(years), Math.Ceiling(months));
+            }
+        }
+    }
+}
