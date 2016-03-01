6911beaf0c56e48ebd752cee1eea9f240c2bb1dd
AlexanderKrustev <a.krustev@steni.bg>
2016-03-01 17:23:52 +0200

    Exam Preparation

---
+++ Exams/26 April 2015/ChessboardGame.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chessboard_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int size = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int totalSize = size*size;
+            int blackTeamScore = 0;
+            int whiteTeamScore = 0;
+
+            if (input.Length < totalSize)
+            {
+                input = input + new string('_', totalSize - input.Length);
+            }
+
+            for (int i = 0; i < totalSize; i++)
+            {
+                if (((int) input[i] >= 48 && (int) input[i] <= 58) || ((int) input[i] >= 65 && (int) input[i] <= 90) || ((int) input[i] >= 97 && (int) input[i] <= 122))
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (((int) input[i] >= 65 && (int) input[i] <= 90))
+                        {
+                            whiteTeamScore += (int) input[i];
+                        }
+                        else
+                        {
+                            blackTeamScore += (int) input[i];
+                        }
+
+                    }
+                    else
+                    {
+                        if (((int) input[i] >= 65 && (int) input[i] <= 90))
+                        {
+                            blackTeamScore += (int) input[i];
+                        }
+                        else
+                        {
+                            whiteTeamScore += (int) input[i];
+                        }
+                    }
+                }
+            }
+            if (blackTeamScore > whiteTeamScore)
+            {
+                Console.WriteLine("The winner is: black team");
+                Console.WriteLine(blackTeamScore-whiteTeamScore);
+
+            }
+            else if (blackTeamScore < whiteTeamScore)
+            {
+                Console.WriteLine("The winner is: white team");
+                Console.WriteLine(whiteTeamScore-blackTeamScore);
+            }
+            else if (blackTeamScore == whiteTeamScore)
+            {
+                Console.WriteLine("Equal result: {0}", blackTeamScore);
+            }
+        }
+    }
+}
+
+
+
+    
+
---
+++ Exams/26 April 2015/KingOfThieves.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingOfThieves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n/2+1; i++)  //first part to middle +1 addiional row
+            {
+                Console.Write(new string(' ',((n-1) / 2)-i));  //the ammount of the "white" symbols before the symbol
+                Console.Write(new string(symbol, i * 2 + 1)); //the ammount of the symbol
+                Console.Write(new string(' ', ((n - 1) / 2) - i)); //the ammount of the "white" symbols after the symbol
+                Console.WriteLine(); // switch to the new row
+            }
+
+            for (int i = n / 2-1 ; i >= 0 ; i--) //second part 
+            {
+                Console.Write(new string(' ', ((n - 1) / 2) - i)); //same
+                Console.Write(new string(symbol, i * 2 + 1));
+                Console.Write(new string(' ', ((n - 1) / 2) - i));
+                Console.WriteLine();
+            }
+        }
+    }
+}
