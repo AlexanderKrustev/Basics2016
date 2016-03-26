6e4e2b43360506b28ab75321c5f73a5e7f2ecd7d
AlexanderKrustev <a.krustev@steni.bg>
2016-03-26 13:23:55 +0200

    26 March ExamPreparation

---
+++ Exams/26MarchPreparations/LightTheTorches.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    using System.Runtime.InteropServices;
+
+    class Program
+    {
+        static void Main()
+        {
+            int numberRooms = int.Parse(Console.ReadLine());
+            string stateOfRooms = Console.ReadLine();
+            string[] roomsArray = new string[numberRooms];
+            for (int i = 0; i < roomsArray.Length; i++)
+            {
+               
+                roomsArray[i] = stateOfRooms[i%stateOfRooms.Length].ToString();
+              
+            }
+            string command = Console.ReadLine();
+            int currentPossition = roomsArray.Length/2;
+            while (command != "END")
+            {
+                string[] agrs = command.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                int step = int.Parse(agrs[1]) + 1;
+
+                if (command.Contains("LEFT"))
+                {
+                    if (currentPossition == 0)
+                    {
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                    currentPossition -= step;
+                    if (currentPossition < 0)
+                    {
+                        currentPossition = 0;
+                    }
+                }
+                else
+                {
+                    if (currentPossition == roomsArray.Length-1)
+                    {
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                    currentPossition += step;
+
+                    if (currentPossition > roomsArray.Length-1)
+                    {
+                        currentPossition = roomsArray.Length-1;
+                        
+                    }
+                }
+
+                if (roomsArray[currentPossition] == "D")
+                {
+                    roomsArray[currentPossition] = "L";
+                }
+                else
+                {
+                    roomsArray[currentPossition] = "D";
+                }
+                command = Console.ReadLine();
+            }
+
+           // Console.WriteLine(string.Join(" ", roomsArray));
+            int counter = 0;
+            for (int i = 0; i < roomsArray.Length; i++)
+            {
+                if (roomsArray[i] == "D")
+                {
+                    counter++;
+                }
+            }
+
+            int count = roomsArray.Count(c => c == "D");
+            Console.WriteLine(counter*68);
+           
+
+        }
+    }
+}
---
+++ Exams/26MarchPreparations/PassionDays.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line
+{
+    using System.Net.Configuration;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal money = decimal.Parse(Console.ReadLine());
+            String command = Console.ReadLine();
+            int countOfPurchases = 0;
+            while(command != "mall.Enter")
+            {
+                command = Console.ReadLine();
+            }
+            command = Console.ReadLine();
+
+            while (command != "mall.Exit")
+            {
+                for (int i = 0; i < command.Length; i++)
+                {
+                    decimal price = 0;
+
+                    if (char.IsLower(command[i]))
+                    {
+                        price = command[i]*0.3m;
+                        if (money < price)
+                        {
+                            continue;
+                        }
+                        money -= price;
+                        countOfPurchases++;
+                    }
+                    else if (char.IsUpper(command[i]))
+                    {
+                        price = command[i] * 0.5m;
+                        if (money < price)
+                        {
+                           continue;
+                        }
+                        money -= price;
+                        countOfPurchases++;
+                    }
+                    else if (command[i] == '%')
+                    {
+                        if (money <= 0)
+                        {
+                            continue;
+                        }
+                        money = money/2m;
+                        countOfPurchases++;
+                    }
+                    else if (command[i] == '*')
+                    {
+                        money += 10;
+                    }
+
+                    else
+                    {
+                        price = command[i];
+                        if (money < price)
+                        {
+                            continue;
+                        }
+                        money -= price;
+                        countOfPurchases++;
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            if (countOfPurchases == 0)
+            {
+                Console.WriteLine("No purchases. Money left: {0:f2} lv.", money);
+            }
+            else
+            {
+                Console.WriteLine("{0} purchases. Money left: {1:f2} lv.", countOfPurchases, money);
+            }
+
+        }
+    }
+}
