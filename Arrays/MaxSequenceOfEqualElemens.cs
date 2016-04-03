a5b9073e15a0f99b5fc0b8a37ff731f1a5da5adb
AlexanderKrustev <a.krustev@steni.bg>
2016-04-03 11:36:30 +0300

    Max Seq

---
+++ Arrays/MaxSequenceOfEqualElemens.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSequenceOfEqualElemens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = new List<int>();
+            input = Console.ReadLine().
+                Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> temp = new List<int>();
+            List<int> finalResut=new List<int>();
+            temp.Add(input[0]);
+            for (int i = 1; i < input.Count; i++)
+            {
+                if (input[i] == temp[temp.Count-1])
+                {
+                    temp.Add(input[i]);
+                }
+                else
+                {
+                    if (temp.Count > finalResut.Count)
+                    {
+                        finalResut.Clear();
+                        for (int j = 0; j < temp.Count; j++)
+                        {
+                           finalResut.Add(temp[j]); 
+                        }
+                        temp.Clear();
+                        temp.Add(input[i]);
+                    }
+                    else
+                    {
+                        temp.Clear();
+                        temp.Add(input[i]);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ",finalResut));
+        }
+    }
+}
