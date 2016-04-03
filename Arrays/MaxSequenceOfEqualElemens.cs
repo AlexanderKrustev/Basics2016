44f555c6b9774e17d2bc4cda1322415ee92fd195
AlexanderKrustev <a.krustev@steni.bg>
2016-04-03 11:37:55 +0300

    max

--- Arrays/MaxSequenceOfEqualElemens.cs
+++ Arrays/MaxSequenceOfEqualElemens.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MaxSequenceOfEqualElemens
-{
-    class Program
+    class MaxSequenceOfEqualElemens
     {
         static void Main(string[] args)
         {
@@ -17,33 +13,34 @@ namespace MaxSequenceOfEqualElemens
                 .ToList();
             List<int> temp = new List<int>();
             List<int> finalResut=new List<int>();
-            temp.Add(input[0]);
-            for (int i = 1; i < input.Count; i++)
+            temp.Add(input[0]); //initialize the first element on the first subseq
+            for (int i = 1; i < input.Count; i++) // iterate over input
             {
-                if (input[i] == temp[temp.Count-1])
-                {
+                if (input[i] == temp[temp.Count-1]) // if the current element == with the previous one 
+                {                                   // we add the current one in temp list
                     temp.Add(input[i]);
                 }
-                else
+                else // if the seq is over start check if it is max seq
                 {
-                    if (temp.Count > finalResut.Count)
+                    if (temp.Count > finalResut.Count) //if temp has more items than the final list, opy temp to final
                     {
-                        finalResut.Clear();
+                        finalResut.Clear(); // clear the old result
+
                         for (int j = 0; j < temp.Count; j++)
                         {
-                           finalResut.Add(temp[j]); 
+                           finalResut.Add(temp[j]);  // copy temp to final
                         }
-                        temp.Clear();
-                        temp.Add(input[i]);
+                        temp.Clear(); // clear them we already have temp into the result
+                        temp.Add(input[i]); // initialize the next element on temp
                     }
                     else
                     {
-                        temp.Clear();
-                        temp.Add(input[i]);
+                        temp.Clear(); // if the current seq is over and it does not have more elements that the previous checked seq we clear it
+                        temp.Add(input[i]); // initialize the next element on temp
                     }
                 }
             }
             Console.WriteLine(string.Join(" ",finalResut));
         }
     }
-}
+
